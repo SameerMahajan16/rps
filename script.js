a6e710fef37626b088a8af6eaae9a8de05abefc1
sameer16 <Samneomah@gmail>
2025-01-13 10:25:18 -0500

    initisl commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Get the user's choice
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (
+      userInput === "rock" ||
+      userInput === "paper" ||
+      userInput === "scissors" ||
+      userInput === "bomb"
+    ) {
+      return userInput;
+    } else {
+      console.log("Error: Invalid input");
+    }
+  };
+  
+  // Get the computer's choice
+  const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return "rock";
+      case 1:
+        return "paper";
+      case 2:
+        return "scissors";
+    }
+  };
+  
+  // Determine the winner
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === "bomb") {
+      return "User wins with a secret cheat code!";
+    }
+    if (userChoice === computerChoice) {
+      return "The game is a tie!";
+    }
+    if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+        return "Computer wins!";
+      } else {
+        return "User wins!";
+      }
+    }
+    if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+        return "Computer wins!";
+      } else {
+        return "User wins!";
+      }
+    }
+    if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+        return "Computer wins!";
+      } else {
+        return "User wins!";
+      }
+    }
+  };
+  
+  // Play the game
+  function playGame(userChoice) {
+    const computerChoice = getComputerChoice();
+    console.log(`User choice: ${userChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+  };
+  
+  
+
+  
+  
+  
